82f45eebd87f2839ba699ae74491315430dd82ef
Rudy Torres <thalorudy26@gmail.com>
2024-09-11 17:11:28 -0500

    Version inicial del proyecto project-jpa

---
+++ src/main/java/com/devpredator/project_jpa/App.java
@@ -0,0 +1,10 @@
+package com.devpredator.project_jpa;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
